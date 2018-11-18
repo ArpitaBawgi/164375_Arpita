dc42a0719abcad33a05b8095da778534658ccc72
ArpitaBawgi <44159231+ArpitaBawgi@users.noreply.github.com>
2018-11-18 20:14:16 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,15 @@
+package com.arpita.oops;
+
+public class Abstract extends TestAbstract {
+                  
+	   public void draw() {
+		   System.out.println("Drawing a circle");
+	   }
+	
+	public static void main(String[] args) {
+		Abstract abstract1= new Abstract();
+		abstract1.display();
+
+	}
+
+}
---
+++ TestAbstract.java
@@ -0,0 +1,11 @@
+package com.arpita.oops;
+
+public  abstract class TestAbstract {
+	abstract public void draw();
+
+	
+	public void display() {
+		System.out.println("This is circle");
+		
+	}
+}
