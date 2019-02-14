f146366ff98eff71690a996c97cc6ab48d930e63
ArpitaBawgi <44159231+ArpitaBawgi@users.noreply.github.com>
2019-02-14 15:28:01 +0530

    angularjs

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [{path:"create",component:                                                                                                                                                      LoginComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
